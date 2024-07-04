f4e9ba8c3c9ab2b820f06465cfaeee0202e28b08
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-05 01:55:44 +0330

    create new account form and hook

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import {
    Sheet,
    SheetContent,
@@ -7,9 +8,26 @@ import {
 } from "@/components/ui/sheet";
 import React from "react";
 import { useNewAccount } from "../hooks/use-new-account";
+import AccountForm from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { useCreateAccount } from "../api/use-create-account";
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
 
 const NewAccountSheet = () => {
    const { isOpen, onClose } = useNewAccount();
+
+   const mutation = useCreateAccount();
+
+   const onSubmit = (values: FormValues) => {
+      mutation.mutate(values, {
+         onSuccess: () => {
+            onClose();
+         },
+      });
+   };
    return (
       <Sheet open={isOpen} onOpenChange={onClose}>
          <SheetContent className="space-y-4">
@@ -19,6 +37,11 @@ const NewAccountSheet = () => {
                   Create a new Account to track your transactions
                </SheetDescription>
             </SheetHeader>
+            <AccountForm
+               onSubmit={onSubmit}
+               disabled={mutation.isPending}
+               defaultValues={{ name: "" }}
+            />
          </SheetContent>
       </Sheet>
    );
