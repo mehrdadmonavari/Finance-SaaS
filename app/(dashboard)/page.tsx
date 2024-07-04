ed5153fc06fa5015a937c7fc1a6cb95763b81157
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-04 23:57:24 +0330

    create new account sheet and sheet provider

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,20 @@
-import { UserButton } from "@clerk/nextjs";
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import React from "react";
 
 const DashboardPage = () => {
-   return <div>
-      <UserButton afterSwitchSessionUrl="/" />
-   </div>;
+   const { onOpen } = useNewAccount();
+
+   return (
+      <div>
+         <div className="py-6 font-semibold text-2xl">
+            this is Home page!
+         </div>
+         <Button onClick={onOpen}>create new account</Button>
+      </div>
+   );
 };
 
 export default DashboardPage;
---
+++ providers/sheet-provider.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import NewAccountSheet from "@/features/accounts/components/new-account-sheet";
+import React from "react";
+import { useMountedState } from "react-use";
+
+const SheetProvider = () => {
+   const isMounted = useMountedState();
+
+   if (!isMounted) {
+      return null;
+   }
+
+   return (
+      <div>
+         <NewAccountSheet />
+      </div>
+   );
+};
+
+export default SheetProvider;
