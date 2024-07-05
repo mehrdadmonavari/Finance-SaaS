a9123c65d5d9d4e930b3fa0ae1902ec551721aaf
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-05 22:26:15 +0330

    create POST API for bulk delete accounts

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -2,8 +2,9 @@ import { db } from "@/db/drizzle";
 import { accounts, insertAccountSchema } from "@/db/schema";
 import { Hono } from "hono";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-import { eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 import { createId } from "@paralleldrive/cuid2";
 
 const app = new Hono()
@@ -39,6 +40,33 @@ const app = new Hono()
             .insert(accounts)
             .values({ id: createId(), userId: auth.userId, ...values })
             .returning();
+
+         return c.json({ data });
+      }
+   )
+   .post(
+      "/bulk-delete",
+      clerkMiddleware(),
+      zValidator(
+         "json",
+         z.object({
+            ids: z.array(z.string()),
+         })
+      ),
+      async (c) => {
+         const auth = getAuth(c);
+         const values = c.req.valid("json");
+
+         if (!auth?.userId) {
+            return c.json({ error: "Unauthorized" }, 401);
+         }
+
+         const data = await db
+            .delete(accounts)
+            .where(
+               and(eq(accounts.userId, auth.userId), inArray(accounts.id, values.ids))
+            )
+            .returning({ id: accounts.id });
 
          return c.json({ data });
       }
