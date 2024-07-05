1b2bcba18289b282c7c8fab5a0189bdc8a026a8b
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-05 22:04:38 +0330

    modify accounts data-table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -3,28 +3,34 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Plus } from "lucide-react";
+import { Loader2, Plus } from "lucide-react";
 import React from "react";
-import { columns, Payment } from "./column";
+import { columns } from "./column";
 import { DataTable } from "../_components/data-table";
-
-const data: Payment[] = [
-   {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-   },
-   {
-      id: "728ed52f",
-      amount: 70,
-      status: "success",
-      email: "m2@example.com",
-   },
-];
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+import { Skeleton } from "@/components/ui/skeleton";
 
 const Account = () => {
    const newAccount = useNewAccount();
+   const accountsQuery = useGetAccounts();
+   const accounts = accountsQuery.data || [];
+
+   if (accountsQuery.isLoading) {
+      return (
+         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+               <CardHeader>
+                  <Skeleton className="h-8 w-48" />
+               </CardHeader>
+               <CardContent>
+                  <div className="h-[500px] w-full flex items-center justify-center">
+                     <Loader2 className="size-6 text-slate-300 animate-spin" />
+                  </div>
+               </CardContent>
+            </Card>
+         </div>
+      );
+   }
 
    return (
       <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
@@ -40,7 +46,7 @@ const Account = () => {
                   disabled={false}
                   onDelete={() => {}}
                   columns={columns}
-                  data={data}
+                  data={accounts}
                   filterKey="email"
                />
             </CardContent>
