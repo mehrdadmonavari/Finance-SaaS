58ec3d8c486ab5fe931f970a8d0d7fc976744654
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-07 12:49:03 +0330

    create transactions feature api hooks

---
+++ features/transactions/api/use-edit-transaction.ts
@@ -0,0 +1,32 @@
+import { InferResponseType, InferRequestType } from "hono";
+import { client } from "@/lib/hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<(typeof client.api.transactions)[":id"]["$patch"]>;
+type RequestType = InferRequestType<
+   (typeof client.api.transactions)[":id"]["$patch"]
+>["json"];
+
+export const useEditTransaction = (id?: string) => {
+   const queryClient = useQueryClient();
+   const mutation = useMutation<ResponseType, Error, RequestType>({
+      mutationFn: async (json) => {
+         const response = await client.api.transactions[":id"]["$patch"]({
+            param: { id },
+            json,
+         });
+         return await response.json();
+      },
+      onSuccess: () => {
+         toast.success("Transactions updated");
+         queryClient.invalidateQueries({ queryKey: ["transaction", { id }] });
+         queryClient.invalidateQueries({ queryKey: ["transactions"] });
+      },
+      onError: () => {
+         toast.error("Failed to update transactions");
+      },
+   });
+
+   return mutation;
+};
