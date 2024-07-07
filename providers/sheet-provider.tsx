3aadfc9f1339436295b1210687fbc52b11a6dd5c
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-07 23:21:08 +0330

    create new-transaction-sheet component

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -4,6 +4,7 @@ import EditAccountSheet from "@/features/accounts/components/edit-account-sheet"
 import NewAccountSheet from "@/features/accounts/components/new-account-sheet";
 import EditCategorySheet from "@/features/categories/components/edit-account-sheet";
 import NewCategorySheet from "@/features/categories/components/new-category-sheet";
+import NewTransactionSheet from "@/features/transactions/components/new-transaction-sheet";
 import React from "react";
 import { useMountedState } from "react-use";
 
@@ -21,6 +22,8 @@ const SheetProvider = () => {
 
          <NewCategorySheet />
          <EditCategorySheet />
+
+         <NewTransactionSheet />
       </div>
    );
 };
