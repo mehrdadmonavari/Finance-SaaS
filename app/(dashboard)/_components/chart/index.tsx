3f7806484a9bf342f4c61ad3b11251cb1ac832a7
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-07-12 17:41:59 +0330

    add chart loading

--- app/(dashboard)/_components/chart/index.tsx
+++ app/(dashboard)/_components/chart/index.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { AreaChart, BarChart3, FileSearch, LineChart } from "lucide-react";
+import { AreaChart, BarChart3, FileSearch, LineChart, Loader2 } from "lucide-react";
 import React, { useState } from "react";
 import { AreaVariant } from "../area-variant";
 import { BarVariant } from "../bar-variant";
@@ -11,6 +11,7 @@ import {
    SelectTrigger,
    SelectValue,
 } from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface ChartProps {
    data?: {
@@ -70,6 +71,22 @@ export const Chart: React.FC<ChartProps> = ({ data = [] }) => {
                   {chartType === "bar" && <BarVariant data={data} />}
                </>
             )}
+         </CardContent>
+      </Card>
+   );
+};
+
+export const ChartLoading = () => {
+   return (
+      <Card className="border-none drop-shadow-sm">
+         <CardHeader className="flex lg:flex-row lg:items-center justify-between space-y-2 lg:space-y-0">
+            <Skeleton className="h-8 w-48" />
+            <Skeleton className="h-8 lg:w-[120px] w-full" />
+         </CardHeader>
+         <CardContent>
+            <div className="h-[350px] w-full flex items-center justify-center">
+               <Loader2 className="h-6 w-6 text-slate-300 animate-spin" />
+            </div>
          </CardContent>
       </Card>
    );
